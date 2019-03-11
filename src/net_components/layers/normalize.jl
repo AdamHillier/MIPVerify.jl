d603fdbf81510ba3f383dd1b1b7568cc5b89263f
Adam Hillier <a.hillier@outlook.com>
2019-03-11 19:16:19 +0000

    Fix normalize

--- src/net_components/layers/normalize.jl
+++ src/net_components/layers/normalize.jl
@@ -15,11 +15,10 @@ function Base.show(io::IO, p::Normalize)
 end
 
 function apply(p::Normalize, x::Array{<:JuMPReal})
-    println("Applying Normalize layer")
-    println("Size of mean, std, x: $(size(p.mean)), $(size(p.std)), $(size(x))")
-    # @assert size(p.mean) == size(p.std) == size(x)
-    output = (x.-p.mean)./p.std
-    return output
+    padded_shape = (ones(Int, ndims(x) - ndims(p.mean))..., size(p.mean)...)
+    m = reshape(p.mean, padded_shape)
+    s = reshape(p.mean, padded_shape)
+    return (x .- m) ./ s
 end
 
 (p::Normalize)(x::Array{<:JuMPReal}) = apply(p, x)
